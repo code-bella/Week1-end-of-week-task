7eda1ca17125707fa96251a7ed038c78faaef07a
Anna Lena Baumann <rescht@posteo.de>
2022-02-04 17:41:09 +0100

    Checks implemented.

--- src/main/java/de/codebella/EndOfWeek1Task.java
+++ src/main/java/de/codebella/EndOfWeek1Task.java
@@ -6,15 +6,43 @@ public class EndOfWeek1Task {
             return "Error, password cannot be empty.";
         } else if (password.length() < 5) {
             return "Error, password too short.";
+        } else if (!numberChecker(password)) {
+            return "Error, password must contain a number.";
+        } else if (!lowercaseLetterChecker(password)) {
+            return "Error, password must contain a lowercase letter.";
+        } else if (!uppercaseLetterChecker(password)) {
+            return "Error, password must contain an uppercase letter.";
         }
+        return "Password accepted.";
+    }
+
+    private static Boolean numberChecker(String password) {
+        boolean result = false;
         for (int i=0; i < password.length(); i++) {
-            for (int j=0; j < 10; j++) {
-                if (password.charAt(i) == j) {
-                    return "Password accepted.";
-                }
-                return "Error, password must contain numbers.";
+            if (Character.isDigit(password.charAt(i))) {
+                result = true;
             }
         }
-        return "Error.";
+        return result;
+    }
+
+    private static Boolean lowercaseLetterChecker(String password) {
+        boolean result = false;
+        for (int i=0; i < password.length(); i++) {
+            if (Character.isLowerCase(password.charAt(i))) {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    private static Boolean uppercaseLetterChecker(String password) {
+        boolean result = false;
+        for (int i=0; i < password.length(); i++) {
+            if (Character.isUpperCase(password.charAt(i))) {
+                result = true;
+            }
+        }
+        return result;
     }
 }
--- src/test/java/de/codebella/EndOfWeek1TaskTest.java
+++ src/test/java/de/codebella/EndOfWeek1TaskTest.java
@@ -6,17 +6,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class EndOfWeek1TaskTest {
     @Test
+    void passwordShouldBeFine() {
+        assertEquals("Password accepted.", EndOfWeek1Task.pwdChecker("jkl9djsu43KK"));
+    }
+
+    @Test
     void emptyShouldGiveAnError() {
         assertEquals("Error, password cannot be empty.", EndOfWeek1Task.pwdChecker(""));
     }
 
     @Test
     void tooShortShouldGiveAnError() {
-        assertEquals("Error, password too short.", EndOfWeek1Task.pwdChecker("te8t"));
+        assertEquals("Error, password too short.", EndOfWeek1Task.pwdChecker("te8T"));
     }
 
     @Test
     void noNumbersShouldGiveAnError() {
-        assertEquals("Error, password must contain numbers.", EndOfWeek1Task.pwdChecker("tester"));
+        assertEquals("Error, password must contain a number.", EndOfWeek1Task.pwdChecker("testeR"));
+    }
+
+    @Test
+    void noLowercaseLetterShouldGiveAnError() {
+        assertEquals("Error, password must contain a lowercase letter.", EndOfWeek1Task.pwdChecker("6789278389890"));
+    }
+
+    @Test
+    void noUppercaseLetterShouldGiveAnError() {
+        assertEquals("Error, password must contain an uppercase letter.", EndOfWeek1Task.pwdChecker("tester67896798"));
     }
 }
