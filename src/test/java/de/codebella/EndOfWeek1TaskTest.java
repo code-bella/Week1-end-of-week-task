e8d24ce1cba4e44f22d9e393775525d0dd7337b8
Anna Lena Baumann <rescht@posteo.de>
2022-02-04 18:51:35 +0100

    changed test passwords to incorporate all possibilities

--- src/test/java/de/codebella/EndOfWeek1TaskTest.java
+++ src/test/java/de/codebella/EndOfWeek1TaskTest.java
@@ -17,21 +17,21 @@ class EndOfWeek1TaskTest {
 
     @Test
     void tooShortShouldGiveAnError() {
-        assertEquals("Error, password too short.", EndOfWeek1Task.pwdChecker("te8T", 5));
+        assertEquals("Error, password too short.", EndOfWeek1Task.pwdChecker("t$8T", 5));
     }
 
     @Test
     void noNumbersShouldGiveAnError() {
-        assertEquals("Error, password must contain a number.", EndOfWeek1Task.pwdChecker("testeR", 5));
+        assertEquals("Error, password must contain a number.", EndOfWeek1Task.pwdChecker("testeR$", 5));
     }
 
     @Test
     void noLowercaseLetterShouldGiveAnError() {
-        assertEquals("Error, password must contain a lowercase letter.", EndOfWeek1Task.pwdChecker("6789278389890", 5));
+        assertEquals("Error, password must contain a lowercase letter.", EndOfWeek1Task.pwdChecker("6789278389890$A", 5));
     }
 
     @Test
     void noUppercaseLetterShouldGiveAnError() {
-        assertEquals("Error, password must contain an uppercase letter.", EndOfWeek1Task.pwdChecker("tester67896798", 5));
+        assertEquals("Error, password must contain an uppercase letter.", EndOfWeek1Task.pwdChecker("tester67896798$", 5));
     }
 }
